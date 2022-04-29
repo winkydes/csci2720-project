fb4c68a23fe2392d18610e467a4587f7e090d521
Keith Lam <keithlam0110@gmail.com>
2022-04-29 17:58:48 +0800

    basic login page setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import * as React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import Home from "./components/Home"
+import { Routes, Route, Link, Redirect } from "react-router-dom";
+import Home from "./components/Home";
+import LoginPage from "./components/LoginPage";
 import './App.css';
 
 function App() {
+  const [loggedIn, setLoggedIn] = React.useState(false);
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={loggedIn? <Home /> : <LoginPage/>}/>
       </Routes>
     </div>
   );
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,20 @@
 function LoginPage() {
     return(
-        <div className="bg-light">
-            hi
+        <div className="h-100 d-flex justify-content-center align-items-center flex-column">
+            <h2>Login!</h2>
+            <form className="d-flex flex-column">
+                <div>
+                    <label className="m-2">Username:</label>
+                    <input type="text"></input>
+                </div>
+                <div>
+                    <label className="m-2">Password:</label>
+                    <input type="text"></input>
+                </div>
+                <button>Login!</button>
+            </form>
         </div>
     );
 };
+
+export default LoginPage;
