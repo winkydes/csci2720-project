af5bde8fd3927af743953b43ae9d7522d7aedc59
Cheuk Hin HO <cheukhinhojerry@gmail.com>
2022-05-07 13:26:13 +0800

    trial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 function Home(props) {
   return (
     <div>
-      <div className="bg-light">This is homepage.</div>
+      <div className="bg-light">This is homepage. Try commit.</div>
       <button onClick={() => props.callback(false)}>Logout</button>
     </div>);
 }
