4ea91c9ee520bbe866fddb8348366a8a2afaddfb
winkydes <keithlam0110@gmail.com>
2022-05-12 15:32:42 +0800

    table search function bug fix

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { MDBDataTable } from 'mdbreact';
 
 function Table(props) {
   //console.log(props.data)
-  return <MDBDataTable striped bordered small data={props.data} />;
+  return <MDBDataTable striped bordered small data={props.data} sortRows={[]}/>;
 }
 export default Table;
