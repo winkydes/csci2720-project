2591dfb4265a165c54dab6df8669c115b8d76645
Cheuk Hin HO <cheukhinhojerry@gmail.com>
2022-05-12 23:10:14 +0800

    minor fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import {React,useEffect} from 'react';
+import React ,{useEffect} from 'react';
 import { Link} from 'react-router-dom';
 //import{usePapaParse} from 'react-papaparse'
 import Table from './Table';
