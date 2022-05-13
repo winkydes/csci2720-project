9955aa115a34802aa513b3d6ae1c2a68523b381a
winkydes <keithlam0110@gmail.com>
2022-05-13 23:02:51 +0800

    added comments timestamp

--- src/components/CommentBlock.js
+++ src/components/CommentBlock.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 function CommentBlock(props) {
   return (
-    // <div className="bg-info me-2 my-2 p-2 rounded">
-    //   <h6>{props.comment.username}</h6>
-    //   <p>{props.comment.content}</p>
-    // </div>
     <div className="card m-2">
       <div className="card-body">
-        <h5 className="card-title">{props.comment.username}</h5>
-        <p className="card-text">{props.comment.content}</p>
+        <div className="d-flex flex-row">
+          <h6 className="card-title">{props.comment.username}</h6>
+        </div>
+        <p className="card-text d-flex">{props.comment.content}</p>
+        <p className="card-text" style={{fontSize: "10px"}}><em>{props.comment.date.toLocaleString().substring(0, 10)}</em></p>
       </div>
     </div>
   );
